7606efc0fc14a5c8315b3b6cf4956cbe5cf8fbe4
Felkim <fipojaq2005@gmail.com>
2025-04-08 15:23:24 -0500

    Flashcard Header

---
+++ include/flashcard.h
@@ -0,0 +1,19 @@
+#include <string>
+
+class Flashcard
+{
+public:
+    Flashcard(int id, const std::string &question, const std::string &answer);
+
+    int getId() const;
+    std::string getQuestion() const;
+    std::string getAnswer() const;
+
+    void setQuestion(const std::string &question);
+    void setAnswer(const std::string &answer);
+
+private:
+    int id;
+    std::string question;
+    std::string answer;
+};
