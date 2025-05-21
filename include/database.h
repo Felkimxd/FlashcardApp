de695f3008c3eed4f57bbd64f7da78ebfaaeb44e
Felkim <fipojaq2005@gmail.com>
2025-05-20 23:15:18 -0500

    Works properly

--- include/database.h
+++ include/database.h
@@ -7,7 +7,9 @@ enum tables
 {
     Flashcards = 1,
     Users = 2,
-    Game = 3
+    Game = 3,
+    Subject = 4,
+
 };
 
 struct FlashcardData
@@ -15,71 +17,69 @@ struct FlashcardData
     std::string question;
     std::string answer;
     std::string subject;
-
 };
 
 struct UserData
 {
     std::string username;
     std::string password;
-
 };
 
 struct GameData
 {
     std::string range;
-
 };
 
 using FlashcarQA = std::vector<std::pair<std::string, std::string>>;
-class dataBaseManager{
+class dataBaseManager
+{
 
-    public:
-        dataBaseManager();
+public:
+    dataBaseManager();
 
-        sqlite3 *db;
+    sqlite3 *db;
 
-        void insertRegister(tables tabletype, void *data);
-        void readRegister(tables tableType , std::string const &ID);
-        void deleteRegister(tables tableType, std::string const &ID);
-        void editRegister(tables tableType, std::string const &ID, void *newData);
+    void insertRegister(tables tabletype, void *data);
+    void readRegister(tables tableType, std::string const &ID);
+    void deleteRegister(tables tableType, std::string const &ID);
+    void editRegister(tables tableType, std::string const &ID, void *newData);
 
-        FlashcarQA retrieve_Flashcards(std::string &subject ,int const &flashcardQuantity);
+    FlashcarQA retrieve_Flashcards(std::string &subject, int const &flashcardQuantity);
 
-    private:
+private:
+    void createTables();
+
+    const char *DBNAME = "C:\\CARPETAS IMPORTANTES\\Carpetas\\FlashcardApp\\archives\\data\\FlashcardApp.db";
+
+    const char *createFlashcards =
+        "CREATE TABLE IF NOT EXISTS Flashcards ("
+        "ID INTEGER PRIMARY KEY, "
+        "Subject TEXT, "
+        "Question TEXT, "
+        "Answer TEXT, "
+        "Grade INTEGER"
+        ");";
+
+    const char *createUser =
+        "CREATE TABLE IF NOT EXISTS User ("
+        "ID INTEGER PRIMARY KEY, "
+        "Username TEXT, "
+        "Password TEXT"
+        ");";
+
+    const char *createGame =
+        "CREATE TABLE IF NOT EXISTS Game ("
+        "ID INTEGER PRIMARY KEY, "
+        "Range TEXT);";
+
+    const std::string insertFlashcardQuery =
+        "INSERT INTO Flashcards (Question, Answer,Subject,Grade) VALUES (?, ?, ?,NULL);";
+
+    const std::string insertUsersQuery =
+        "INSERT INTO Users (Username,Password) VALUES (?,?);";
+
+    const std::string insertGameQuery =
+        "INSERT INTO Game (Range) VALUES (?);";
 
 
-        void createTables();
-
-        const char *DBNAME = "C:\\CARPETAS IMPORTANTES\\Carpetas\\FlashcardApp\\archives\\data\\FlashcardApp.db";
-
-        const char *createFlashcards =
-            "CREATE TABLE IF NOT EXISTS Flashcards ("
-            "ID INTEGER PRIMARY KEY, "
-            "Subject TEXT, "
-            "Question TEXT, "
-            "Answer TEXT, "
-            "Grade INTEGER"
-            ");";
-
-        const char *createUser =
-            "CREATE TABLE IF NOT EXISTS User ("
-            "ID INTEGER PRIMARY KEY, "
-            "Username TEXT, "
-            "Password TEXT"
-            ");";
-
-        const char *createGame =
-            "CREATE TABLE IF NOT EXISTS Game ("
-            "ID INTEGER PRIMARY KEY, "
-            "Range TEXT);";
-
-        const std::string insertFlashcardQuery =
-            "INSERT INTO Flashcards (Question, Answer,Subject,Grade) VALUES (?, ?, ?,NULL);";
-
-        const std::string insertUsersQuery =
-            "INSERT INTO Users (Username,Password) VALUES (?,?);";
-
-        const std::string insertGameQuery =
-            "INSERT INTO Game (Range) VALUES (?);";
 };
--- src/database.cpp
+++ src/database.cpp
@@ -249,7 +249,3 @@ void dataBaseManager::deleteRegister(tables tableType, const std::string &ID)
     sqlite3_finalize(stmt);
     sqlite3_close(this->db);
 }
-
-FlashcarQA retrieve_Flashcards(std::string &subject, int const &flashcardQuantity){
-    
-}
